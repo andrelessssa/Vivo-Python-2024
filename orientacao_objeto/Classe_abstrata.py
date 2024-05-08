e2a423f3208f385829115781c96e64f9a994f61a
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2024-05-08 18:51:07 -0300

    :construction:  Criadas as classes abstratas
    :construction:  Criadas as classes abstratas

---
+++ orientacao_objeto/Classe_abstrata.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod # importando a classe ABX
+
+class ControleRemoto(ABC):   #Aqui eu falo que é uma extencao de ABC
+    @abstractmethod  #Essa função abstrata tem que ser implementada em qualquer classe filha.
+    def ligar(self):      # o metodo abstrato nao vai ter um corpo e as classes filhas sao obrigadas a implementar
+        pass
+    @abstractmethod    
+    def desligar(self):
+        pass        
+
+class ControleTv(ControleRemoto): # agora eu soi obrigado a implementar esses dois metodos
+    def ligar(self):
+        print("Ligando a TV")
+        
+
+    def desligar(self):
+        print("Desligando a TV...")
+
+
+class ContrleArCondicionado(ControleRemoto):
+    def ligar(self):
+        print("Ligando Ar Condicionado")
+
+    def desligar(self):
+        print("desligando...")   
+
+
+
+
+controle = ControleTv()
+controle.ligar()
+controle.desligar()
+
+controle = ContrleArCondicionado()
+controle.ligar()
